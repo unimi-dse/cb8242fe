b9bc76b93f2d2b626c69a7da35b177344f30a279
Serafina-Z <59978350+Serafina-Z@users.noreply.github.com>
2020-02-09 19:20:56 +0100

    Add files via upload

--- R/dseprojectS.R
+++ R/dseprojectS.R
@@ -2,26 +2,24 @@
 
 #' @description Plot the open, the close and the adjusted price of the stock and give a simple stock return.
 
-#' @param x Desription of argument
-
-#' @return NULL
+#' @param symbol NULL
 
 #' @import utils
 
 #' @export stock_simple_return
 
-stock_simple_return<-function(x)
+stock_simple_return<-function(symbol)
 {
 
   #To ignore the warnings during usage
 
   options(warn=-1)
-
   options("getSymbols.warning4.0"=FALSE)
 
-  #Importing price data for the givensymbol
+  #Importing price data for the given symbol
 
-  data <- quantmod::getSymbols(Symbols = 'x', src = 'yahoo', auto.assign = FALSE)
+  v <- "symbol"
+  data <- quantmod::getSymbols(Symbols = "v", src = 'yahoo', warnings = FALSE, auto.assign = FALSE)
 
   #Transforming the XTS to the data frame
 
@@ -35,7 +33,6 @@ stock_simple_return<-function(x)
   priceplot <- ggplot2::ggplot()+ggplot2::geom_line(data = data,ggplot2::aes(x = data[,1],y = data[,2], colour = "price.Open"),size=1)+ggplot2::geom_line(data = data,ggplot2::aes(x =data[,1],y = data[,5],colour ="price.Close"),size=1) + ggplot2::geom_line(data = data,ggplot2::aes(x = data[,1],y = data[,7], colour ="price.Adjusted"),size=1) + ggplot2::scale_colour_manual("",values = c("price.Open" = "blue","price.Close" = "green","price.Adjusted" = "red"))+ggplot2::labs(title="Price trend")+ggplot2::xlab("Time")+ggplot2::ylab("Price")
 
   #Calculating stock return
-  simplereturn <- (data[,5]-data[,2])/data[,2]
   result <- merge(data[,1], (data[,5]-data[,2])/data[,2])
   names(result) <- c("Latest date", "Simple stock return")
   newresult <- tail(result,1)
