dfb04c97bbf60fd3ca65bd2bc43272e576bb5f3c
Serafina-Z <59978350+Serafina-Z@users.noreply.github.com>
2020-02-15 11:04:04 +0100

    Add files via upload

--- R/dseprojectS.R
+++ R/dseprojectS.R
@@ -1,14 +1,14 @@
 #' @title Plot and Simpe return
 
-#' @description Plot the open, the close and the adjusted price of the stock and give a simple stock return.
+#' @description Get data from the package
 
-#' @param symbol Symbol of stock
+#' @param x Symbol of stock
 
 #' @import utils
 
-#' @export stock_simple_return
+#' @export data_import
 
-stock_simple_return<-function(symbol)
+data_import<-function(x)
 {
 
   #To ignore the warnings during usage
@@ -18,20 +18,38 @@ stock_simple_return<-function(symbol)
 
   #Importing price data for the given symbol
 
-  v <- "symbol"
-  data1 <- quantmod::getSymbols(Symbols = "v", src = 'yahoo', from="2019-01-01", warnings = FALSE, auto.assign = FALSE)
+  data1 <- quantmod::getSymbols(Symbols = x, src = "yahoo",auto.assign =FALSE, from="2018-01-01")
 
   #Transforming the XTS to the data frame
 
   data2 <- data.frame(date=zoo::index(data1), zoo::coredata(data1))
+  return(data2)
+
+}
+
+#' @title Plot and Simpe return
+
+#' @description Plot the open, the close and the adjusted price of the stock and give a simple stock return.
+
+#' @param x Symbol of stock
+
+#' @import utils
+
+#' @export stock_simple_return
+
+stock_simple_return<-function(x)
+{
+
+  #Importing data
+  data3 <- data_import(x)
 
   #Calculating stock return
-  result <- merge(data2[,1], (data2[,5]-data2[,2])/data2[,2])
+  result <- merge(data3[,1], (data3[,5]-data3[,2])/data3[,2],row.names=FALSE)
   names(result) <- c("Latest date", "Simple stock return")
   newresult <- tail(result,1)
 
   #Getting result
-  print(newresult, row.names=FALSE)
+  return(newresult)
 
 }
 
@@ -40,33 +58,22 @@ stock_simple_return<-function(symbol)
 
 #' @description Plot the open, the close and the adjusted price of the stock and give a simple stock return.
 
-#' @param symbol Symbol of stock
+#' @param x Symbol of stock
 
 #' @import utils
 
 #' @export stock_plot_return
 
-stock_plot_return<-function(symbol)
+stock_plot_return<-function(x)
 {
-  #To ignore the warnings during usage
 
-  options(warn=-1)
-  options("getSymbols.warning4.0"=FALSE)
-
-  #Importing price data for the given symbol
-
-  v <- "symbol"
-  data3 <- quantmod::getSymbols(Symbols = "v", src = 'yahoo', from="2018-01-01", warnings = FALSE, auto.assign = FALSE)
-
-  #Transforming the XTS to the data frame
-
-  data4 <- data.frame(date=zoo::index(data3), zoo::coredata(data3))
+  #Importing data
+  data4 <- data_import(x)
 
   #Plotting the data
-
   priceplot <- ggplot2::ggplot()+ggplot2::geom_line(data = data4,ggplot2::aes(x = data4[,1],y = data4[,2], colour = "price.Open"),size=1)+ggplot2::geom_line(data = data4,ggplot2::aes(x =data4[,1],y = data4[,5],colour ="price.Close"),size=1) + ggplot2::geom_line(data = data4,ggplot2::aes(x = data4[,1],y = data4[,7], colour ="price.Adjusted"),size=1) + ggplot2::scale_colour_manual("",values = c("price.Open" = "blue","price.Close" = "green","price.Adjusted" = "red"))+ggplot2::labs(title="Price trend")+ggplot2::xlab("Time")+ggplot2::ylab("Price")
 
   #Getting result
-  print(priceplot)
+  return(priceplot)
 
 }
